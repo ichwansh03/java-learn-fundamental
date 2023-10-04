3ed3ff177f8b14465e5f7f3b38db61001882d7ba
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-04 15:49:24 +0700

    chore: example work manager in thread

---
+++ thread/src/test/java/org/example/TimerTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerTest {
+
+    @Test
+    void delayedJob() throws InterruptedException {
+        var task = new TimerTask(){
+            @Override
+            public void run() {
+                System.out.println("process delayed..");
+            }
+        };
+
+        var timer = new Timer();
+        timer.schedule(task, 2000);
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    void periodicJob() throws InterruptedException {
+        var task = new TimerTask(){
+            @Override
+            public void run() {
+                System.out.println("progress periodic..");
+            }
+        };
+
+        var timer = new Timer();
+        timer.schedule(task,1000,1000);
+
+        Thread.sleep(5000);
+    }
+}
