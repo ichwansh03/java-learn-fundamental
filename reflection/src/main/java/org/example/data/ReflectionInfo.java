db5de7ceb1438837b4e28f557e898f22c93630eb
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-10 05:52:43 +0700

    chore: add annotation reflection

--- reflection/src/main/java/org/example/data/ReflectionInfo.java
+++ reflection/src/main/java/org/example/data/ReflectionInfo.java
@@ -2,8 +2,11 @@ package org.example.data;
 
 import java.lang.annotation.*;
 
+/**
+ * Retention SOURCE digunakan untuk membuat java-docs
+ */
 @Documented
-@Target(ElementType.PACKAGE)
+@Target({ElementType.PACKAGE, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReflectionInfo {
 }
