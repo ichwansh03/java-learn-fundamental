bf389edcd36a446133d42622aa72b91b3b914b1f
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-09 11:41:29 +0700

    chore: add documentation with `package-info.java`

---
+++ reflection/src/main/java/org/example/data/ReflectionInfo.java
@@ -0,0 +1,9 @@
+package org.example.data;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.PACKAGE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ReflectionInfo {
+}
--- reflection/src/test/java/org/example/data/PersonTest.java
+++ reflection/src/test/java/org/example/data/PersonTest.java
@@ -3,6 +3,9 @@ package org.example.data;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 class PersonTest {
@@ -10,7 +13,12 @@ class PersonTest {
     @Test
     void testCreateClass() {
         Class<Person> personClass = Person.class;
+        Package personClassPackage = personClass.getPackage();
 
+        System.out.println(personClassPackage.getName());
+        System.out.println(Arrays.toString(personClassPackage.getAnnotations()));
+        System.out.println(personClass.getSuperclass());
+        System.out.println(Modifier.isPublic(personClass.getModifiers()));
         System.out.println(personClass.descriptorString());
         System.out.println(personClass.getComponentType());
         System.out.println(Arrays.toString(personClass.getDeclaredMethods()));
@@ -29,5 +37,20 @@ class PersonTest {
         Person person = new Person("Ichwan", "ichwan@test.com", 22);
         name.set(person, "Mahmud");
         System.out.println(person.getName());
+    }
+
+    @Test
+    void testGetMethodParameters() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<Person> personClass = Person.class;
+        Method[] methods = personClass.getDeclaredMethods();
+
+        for (Method value: methods){
+            System.out.println(value.getName());
+        }
+
+        Method setName = personClass.getDeclaredMethod("setName", String.class);
+
+        Person person = new Person("Ichwan", "ichwan@test.com", 22);
+        setName.invoke(person,"Abdullah");
     }
 }
